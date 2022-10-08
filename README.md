b93358a4b0ac16f1797c4a310b3cb5b3fca9dfc2
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-08 14:16:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-c17-l8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-3wek34)
---
+++ src/router.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App';
+import Homepage from './pages/Homepage/Homepage';
+import Profile from './pages/Profile/Profile';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import ProfileOverview from './pages/Profile/pages/ProfileOverview/ProfileOverview';
+import ProfileData from './pages/Profile/pages/ProfileData/ProfileData';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: '/profile/:id',
+        element: <Profile />,
+        caseSensitive: true,
+        children: [
+          {
+            index: true,
+            element: <ProfileOverview />,
+          },
+          {
+            path: 'data',
+            element: <ProfileData />,
+          },
+        ],
+      },
+      {
+        path: '/profile/*',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
